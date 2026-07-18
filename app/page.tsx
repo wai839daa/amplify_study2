86a5d84587e41fc08451075cd09b54a17d55c452
DESKTOP-PPDP495\fukum <uel_fukuma@yahoo.co.jp>
2026-07-18 22:10:55 +0900

    cssインポートのエラーを解消

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+//import "./../app/app.css";
+import "./app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+//import "@aws-amplify/ui-react/dist/styles.css";
 import { MutualAuthenticationMode } from "aws-cdk-lib/aws-elasticloadbalancingv2";
 
 Amplify.configure(outputs);
